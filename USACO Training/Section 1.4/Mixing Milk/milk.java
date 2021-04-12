c1c772a1cf1f0b3782d4afe12a6e345127ccfe57
TurtleCamera <80860454+TurtleCamera@users.noreply.github.com>
2021-04-12 14:29:22 -0500

    Add files via upload

--- USACO Training/Section 1.4/Mixing Milk/milk.java
+++ USACO Training/Section 1.4/Mixing Milk/milk.java
@@ -1,43 +1,48 @@
 /*
-ID: zou.all1
+ID: all.zou1
 LANG: JAVA
 TASK: milk
 */
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class milk
 {
-	public static void main(String [] args) throws IOException
+	public static void main (String [] args) throws IOException
 	{
-		Scanner file=new Scanner(new File("milk.in"));
-		PrintWriter out=new PrintWriter(new BufferedWriter(new FileWriter("milk.out")));
-		int units=Integer.parseInt(file.next());
-		int farmercount=Integer.parseInt(file.next());
-		int count=0;
-		TreeMap <Integer,Integer> farmers=new TreeMap <Integer,Integer>(); 
-		int cost=0;
-		while(count<farmercount)
-		{
-			farmers.put(Integer.parseInt(file.next()),Integer.parseInt(file.next()));
-			count++;
-		}
-		for(int f: farmers.keySet())
-		{
-			if(farmers.get(f)<=units)
-			{
-				cost+=f*farmers.get(f);
-				units-=farmers.get(f);
-			}
-			else
-				while(units>0)
-				{
-					cost+=f;
-					units--;
-				}
-		}
-		out.println(cost);
-		out.close();
+	    Scanner in = new Scanner(new File("milk.in"));
+	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("milk.out")));
+	    int totalmilk=in.nextInt();
+	    int totalfarmers=in.nextInt();
+	    int count=0;
+	    int totalcost=0;
+	    HashMap <Integer,Integer> farmers=new HashMap <Integer,Integer>();
+	    while(count<totalfarmers)
+	    {
+	    	int price=in.nextInt();
+	    	if(farmers.containsKey(price))
+	    		farmers.put(price,farmers.get(price)+in.nextInt());
+	    	else	
+	    		farmers.put(price,in.nextInt());
+	    	count++;
+	    }
+	    for (Map.Entry<Integer,Integer> temp : farmers.entrySet())
+	    {
+	    	if(totalmilk>=1)
+	    	{
+	    		if(temp.getValue()>totalmilk)	//If you can buy milk but you don't need to buy all of them.
+	    		{
+	    			totalcost+=temp.getKey()*totalmilk;
+	    			break;
+	    		}
+	    		else	//If you can buy all of the milk.
+	    		{
+	    			totalcost+=temp.getKey()*temp.getValue();
+	    		}
+	    		totalmilk-=temp.getValue();
+	    	}
+	    }
+	    out.println(totalcost);
+	    out.close();
 	}
 }
